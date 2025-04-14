912f2ce2d05119f2a089a77dcd94647693485407
Johannes Rønning <johannes@picterus.com>
2025-04-14 20:52:14 +0200

    super basic start
    spritesheet is a reference

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,129 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
+const rl = @import("raylib");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("jsjam_lib");
+var WINDOW_WIDTH: i32 = 1080;
+var WINDOW_HEIGHT: i32 = 720;
+const RENDER_WIDTH: i32 = 1080;
+const RENDER_HEIGHT: i32 = 720;
+
+const GRID_SIZE: usize = 45;
+
+const tile_type = enum(u8) {
+    tundra = 0,
+    dirt,
+    grass,
+    rock,
+};
+
+const Tile = struct {
+    kind: tile_type = .tundra,
+};
+
+const State = struct {
+    tilemap: [GRID_SIZE * GRID_SIZE]Tile,
+    scene: rl.RenderTexture,
+
+    pub fn init(_: std.mem.Allocator) !State {
+        return .{
+            .tilemap = std.mem.zeroes([GRID_SIZE * GRID_SIZE]Tile),
+            .scene = try rl.loadRenderTexture(RENDER_WIDTH, RENDER_HEIGHT),
+        };
+    }
+
+    pub fn draw_tiles(self: State, sheet: rl.Texture) !void {
+        const tile_width = 32;
+        const tile_height = 16;
+
+        const sheet_rows = try std.math.divFloor(i32, sheet.width, tile_width);
+
+        for (self.tilemap, 0..) |tile, i| {
+            const x: i32 = @intCast(try std.math.mod(usize, i, GRID_SIZE));
+            const y: i32 = @intCast(try std.math.divFloor(usize, i, GRID_SIZE));
+
+            var screen_pos = try project_to_screen(x, y, tile_width, tile_height);
+
+            const half_screen = try std.math.divFloor(f32, RENDER_WIDTH, 2);
+            screen_pos.x += half_screen;
+            screen_pos.y += 20;
+
+            var rand = std.Random.DefaultPrng.init(i);
+            const r = try std.math.mod(u64, rand.next(), @intCast(sheet_rows));
+
+            rl.drawTextureRec(sheet, .{ .x = @floatFromInt(tile_width * r), .y = @as(f32, @floatFromInt(@intFromEnum(tile.kind))) * tile_width, .width = tile_width, .height = tile_width }, screen_pos, .white);
+        }
+    }
+
+    pub fn deinit(_: State, _: std.mem.Allocator) void {}
+};
+
+pub fn main() anyerror!void {
+    setup_window();
+    defer rl.closeWindow();
+
+    const sheet = try rl.loadTexture("spritesheet.png");
+
+    rl.setTargetFPS(60);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var state = try State.init(allocator);
+    defer state.deinit(allocator);
+
+    while (!rl.windowShouldClose()) {
+        state.scene.begin();
+        rl.clearBackground(.dark_gray);
+        try state.draw_tiles(sheet);
+
+        if (rl.isKeyPressed(.e) or rl.isKeyDown(.q)) {
+            const x = std.crypto.random.intRangeAtMost(u32, 0, GRID_SIZE);
+            const y = std.crypto.random.intRangeAtMost(u32, 0, GRID_SIZE);
+
+            if (x * y == GRID_SIZE * GRID_SIZE) {
+                var random_tile = &state.tilemap[x * y - 1];
+                random_tile.kind = .grass;
+            } else {
+                var random_tile = &state.tilemap[x * y];
+                random_tile.kind = .grass;
+            }
+        }
+
+        state.scene.end();
+
+        draw_final_scene(state.scene);
+    }
+}
+
+/// updates the global WINDOW_WIDTH and WINDOW_HEIGHT variables
+fn setup_window() void {
+    rl.setConfigFlags(.{ .window_resizable = true });
+    rl.initWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "jsjam");
+    const monitor = rl.getCurrentMonitor();
+    WINDOW_WIDTH = rl.getMonitorWidth(monitor);
+    WINDOW_HEIGHT = rl.getMonitorHeight(monitor);
+    rl.setWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+}
+
+/// where we draw the final texture on to the screen
+/// it is drawn at a fixed resolution regardless of screen size / dimensions
+fn draw_final_scene(scene: rl.RenderTexture) void {
+    rl.beginDrawing();
+    rl.drawTexturePro(scene.texture, .{
+        .x = 0,
+        .y = 0,
+        .width = @floatFromInt(RENDER_WIDTH),
+        .height = @floatFromInt(-RENDER_HEIGHT),
+    }, .{
+        .x = 0,
+        .y = 0,
+        .width = @floatFromInt(WINDOW_WIDTH),
+        .height = @floatFromInt(WINDOW_HEIGHT),
+    }, rl.Vector2.zero(), 0, .white);
+    rl.endDrawing();
+}
+
+fn project_to_screen(x: i32, y: i32, tile_width: i32, tile_height: i32) !rl.Vector2 {
+    // This makes the grid skewed and look more "angled"
+    return rl.Vector2{
+        .x = @floatFromInt((x - y) * (try std.math.divFloor(i32, tile_width, 2))),
+        .y = @floatFromInt((x + y) * (try std.math.divFloor(i32, tile_height, 2))),
+    };
+}
